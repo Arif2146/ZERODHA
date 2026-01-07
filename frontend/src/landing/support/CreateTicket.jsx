d357a00bed2a815c29429318b1aff15622dd4ec9
Arif Bepari <arifbepari2006@gmail.com>
2026-01-07 14:49:25 +0530

    suppoort com

---
+++ frontend/src/landing/support/CreateTicket.jsx
@@ -0,0 +1,9 @@
+function CreateTicket() {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold underline">CreateTicket</h1>
+        </div>
+    );
+}
+
+export default CreateTicket
---
+++ frontend/src/landing/support/SupportPage.jsx
@@ -0,0 +1,15 @@
+import Hero from "./Hero";
+import Navbar from "../Navbar";
+import CreateTicket from "./CreateTicket";
+
+function SupportPage() {
+    return (
+       <>
+        <Navbar />
+        <Hero /> 
+        <CreateTicket /> 
+       </>
+    );
+}
+
+export default SupportPage
