32c8fb3367d7b4b6a33e3eb2a20e64626536ed3f
Arif Bepari <arifbepari2006@gmail.com>
2026-01-07 14:44:20 +0530

    about section complt

---
+++ frontend/src/landing/about/AboutPage.jsx
@@ -0,0 +1,14 @@
+import Hero from "./Hero";
+import Navbar from "../Navbar";
+import Team from "./Team";
+function AboutPage() {
+    return (
+       <>
+        <Navbar />
+        <Hero />
+        <Team />
+       </>
+    );
+}
+
+export default AboutPage
---
+++ frontend/src/landing/about/Team.jsx
@@ -0,0 +1,9 @@
+function Team() {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold underline">Team</h1>
+        </div>
+    );
+}
+
+export default Team
