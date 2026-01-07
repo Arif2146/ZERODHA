3a039e30dbc6bea729e67334189bcb5ff2599c25
Arif Bepari <arifbepari2006@gmail.com>
2026-01-07 14:39:25 +0530

    file change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,8 @@
-
+import HomePage from "./landing/home/HomePage";
 const App = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <HomePage />
     </div>
   );
 };
