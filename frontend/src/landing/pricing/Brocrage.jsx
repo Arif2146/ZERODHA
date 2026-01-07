03a9cabf2e891ed0deacc541fe20229590457a1c
Arif Bepari <arifbepari2006@gmail.com>
2026-01-07 14:45:52 +0530

    pricing com

---
+++ frontend/src/landing/pricing/Brocrage.jsx
@@ -0,0 +1,9 @@
+function Brocrage() {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold underline">Brocrage</h1>
+        </div>
+    );
+}
+
+export default Brocrage
---
+++ frontend/src/landing/pricing/PriceingPage.jsx
@@ -0,0 +1,14 @@
+import Hero from "./Hero";
+import Navbar from "../Navbar";
+import Brocrage from "./Brocrage";
+function PricingPage() {
+    return (
+       <>
+       <Navbar />
+       <Hero />
+       <Brocrage />
+       </>
+    );
+}
+
+export default PricingPage
