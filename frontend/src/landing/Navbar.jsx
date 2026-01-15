d10ad71f09537dc6037cde2718bdf1979c9eb725
Arif Bepari <arifbepari2006@gmail.com>
2026-01-15 14:03:33 +0530

    navbar

--- frontend/src/landing/Navbar.jsx
+++ frontend/src/landing/Navbar.jsx
@@ -1,9 +1,11 @@
+import Logo from "../assets/logo.svg";
+
 function Navbar() {
   return (
-    <div className="container">
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <nav class="navbar navbar-expand-lg border-bottom" style={{backgroundColor:"#FFF"}}>
+      <div class="container p-2">
         <a class="navbar-brand" href="# ">
-          Navbar
+          <img src={Logo} width="25%" alt="Logo"/>
         </a>
         <button
           class="navbar-toggler"
@@ -16,64 +18,40 @@ function Navbar() {
         >
           <span class="navbar-toggler-icon"></span>
         </button>
-
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="# ">
-                Home <span class="sr-only">(current)</span>
+          <form class="d-flex" role="search">
+            <ul class="navbar-nav mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="# ">
+                Sign Up
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="# ">
-                Link
+              <a class="nav-link active" href="# ">
+                About
               </a>
-            </li>
-            <li class="nav-item dropdown">
-              <a
-                class="nav-link dropdown-toggle"
-                href="# "
-                id="navbarDropdown"
-                role="button"
-                data-toggle="dropdown"
-                aria-haspopup="true"
-                aria-expanded="false"
-              >
-                Dropdown
-              </a>
-              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="# ">
-                  Action
-                </a>
-                <a class="dropdown-item" href="# ">
-                  Another action
-                </a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="# ">
-                  Something else here
-                </a>
-              </div>
             </li>
             <li class="nav-item">
-              <a class="nav-link disabled" href="# ">
-                Disabled
+              <a class="nav-link active" href="# ">
+                Products
+              </a>
+            </li>
+             <li class="nav-item">
+              <a class="nav-link active" href="# ">
+                Pricing
+              </a>
+            </li>
+             <li class="nav-item">
+              <a class="nav-link active" href="# ">
+                Support
               </a>
             </li>
           </ul>
-          <form class="form-inline my-2 my-lg-0">
-            <input
-              class="form-control mr-sm-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
-              Search
-            </button>
           </form>
+          </div>
         </div>
       </nav>
-    </div>
+
   );
 }
 
