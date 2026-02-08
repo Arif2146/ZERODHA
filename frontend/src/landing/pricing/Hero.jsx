d1072af5c570ee9e1f3fb30c0fb5680c09dff9ed
Arif Bepari <arifbepari2006@gmail.com>
2026-02-08 16:18:17 +0530

    pricing Hero complete

--- frontend/src/landing/pricing/Hero.jsx
+++ frontend/src/landing/pricing/Hero.jsx
@@ -1,9 +1,32 @@
+import intradayTrades from "../../assets/intradayTrades.svg";
+import pricing0 from "../../assets/pricing0.svg";
+
 function Hero() {
-    return (
-        <div>
-            <h1 className="text-3xl font-bold underline">Hero</h1>
+  return (
+    <div className="container">
+      <div className="row p-5 mt-5 border-bottom text-center">
+        <h1>Charges</h1>
+        <h3 className="text-muted">List of all charges and taxes</h3>
+      </div>
+      <div className="row p-5 mt-5 text-center">
+        <div className="col-4 p-5">
+          <img src={pricing0} alt="Pricing 0" />
+          <h3 className="text-center mb-4">Free equity delivery</h3>
+          <h5 className="text-muted text-center">All equity delivery investments (NSE, BSE), are absolutely free — ₹ 0 brokerage.</h5>
         </div>
-    );
+        <div className="col-4 p-5">
+          <img src={intradayTrades} alt="Intraday Trades" />
+          <h3 className="text-center mb-4">Intraday and F&O trades</h3>
+          <h5 className="text-muted text-center">Flat ₹ 20 or 0.03% (whichever is lower) per executed order on intraday trades across equity, currency, and commodity trades. Flat ₹20 on all option trades.</h5>
+        </div>
+        <div className="col-4 p-5">
+          <img src={pricing0} alt="Pricing 0" />
+          <h3 className="text-center mb-4">Free direct MF</h3>
+          <h5 className="text-muted text-center">All direct mutual fund investments are absolutely free — ₹ 0 commissions & DP charges.</h5>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Hero
+export default Hero;
--- frontend/src/landing/pricing/PriceingPage.jsx
+++ frontend/src/landing/pricing/PriceingPage.jsx
@@ -1,9 +1,11 @@
 import Hero from "./Hero";
 import Brocrage from "./Brocrage";
+import OpenAcc from './../OpenAcc';
 function PricingPage() {
     return (
        <>
        <Hero />
+       <OpenAcc/>
        <Brocrage />
        </>
     );
