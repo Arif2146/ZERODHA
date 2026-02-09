21e89542ce412384dc2774bb08611a57ec240f40
Arif Bepari <arifbepari2006@gmail.com>
2026-02-09 16:46:10 +0530

    create ticket complete

--- frontend/src/landing/support/CreateTicket.jsx
+++ frontend/src/landing/support/CreateTicket.jsx
@@ -1,9 +1,207 @@
 function CreateTicket() {
-    return (
-        <div>
-            <h1 className="text-3xl font-bold underline">CreateTicket</h1>
+  return (
+    <div className="container">
+      <div className="row p-5 mt-5 mb-5">
+        <h1 className="fs-2">To create a ticket,select a relevant topic</h1>
+        <div className="col-4 p-5 mt-2 mb-2">
+          <h4 className="">
+            <i className="fa fa-plus-circle" aria-hidden="true"></i>
+            Account Opening
+          </h4>
+          <a href=" " style={{ lineHeight: "2.5" }}>
+            Offline Account Opening
+          </a>
+          <br />
+          <a href=" " style={{ lineHeight: "2.5" }}>
+            Company, Partnership and HUF Account
+          </a>
+          <br />
+          <a href=" " style={{ lineHeight: "2.5" }}>
+            Opening
+          </a>
+          <br />
+          <a href=" " style={{ lineHeight: "2.5" }}>
+            NRI Account Opening
+          </a>
+          <br />
+          <a href=" " style={{ lineHeight: "2.5" }}>
+            Online Account Opening
+          </a>
+          <br />
+          <a href=" " style={{ lineHeight: "2.5" }}>
+            Charges at Zerodha
+          </a>
+          <br />
+          <a href=" " style={{ lineHeight: "2.5" }}>
+            Zerodha IDFC FIRST Bank 3-in-1 Account
+          </a>
+          <br />
+          <a href=" " style={{ lineHeight: "2.5" }}>
+            Getting Started
+          </a>
+          <br />
         </div>
-    );
+        <div className="col-4 p-5 mt-2 mb-2">
+          <h4 className="">
+            <i className="fa fa-plus-circle" aria-hidden="true"></i>
+            Your Zerodha Account
+          </h4>
+          <a href=" " style={{ lineHeight: "2.5" }}>
+            Login Credentials
+          </a>
+          <br />
+          <a href=" " style={{ lineHeight: "2.5" }}>
+            Account Modification and Segment Addition
+          </a>
+          <br />
+          <a href=" " style={{ lineHeight: "2.5" }}>
+            DP ID and bank details
+          </a>
+          <br />
+          <a href=" " style={{ lineHeight: "2.5" }}>
+            Your Profile
+          </a>
+          <br />
+          <a href=" " style={{ lineHeight: "2.5" }}>
+            Transfer and conversion of shares
+          </a>
+          <br />
+        </div>
+        <div className="col-4 p-5 mt-2 mb-2">
+          <h4 className="">
+            <i className="fa fa-plus-circle" aria-hidden="true"></i>
+            Your Zerodha Account
+          </h4>
+          <a href=" " style={{ lineHeight: "2.5" }}>
+            Margin/leverage, Product and Order types
+          </a>
+          <br />
+          <a href=" " style={{ lineHeight: "2.5" }}>
+            Kite Web and Mobile
+          </a>
+          <br />
+          <a href=" " style={{ lineHeight: "2.5" }}>
+            Trading FAQsg
+          </a>
+          <br />
+          <a href=" " style={{ lineHeight: "2.5" }}>
+            Corporate Actions
+          </a>
+          <br />
+          <a href=" " style={{ lineHeight: "2.5" }}>
+            Sentinel
+          </a>
+          <br />
+          <a href=" " style={{ lineHeight: "2.5" }}>
+            Kite API
+          </a>
+          <br />
+          <a href=" " style={{ lineHeight: "2.5" }}>
+            Stockreports+
+          </a>
+          <br />
+        </div>
+        <div className="col-4 p-5 mt-2 mb-2">
+          <h4 className="">
+            <i className="fa fa-plus-circle" aria-hidden="true"></i>
+            Account Opening
+          </h4>
+          <a href=" " style={{ lineHeight: "2.5" }}>
+            Offline Account Opening
+          </a>
+          <br />
+          <a href=" " style={{ lineHeight: "2.5" }}>
+            Company, Partnership and HUF Account
+          </a>
+          <br />
+          <a href=" " style={{ lineHeight: "2.5" }}>
+            Opening
+          </a>
+          <br />
+          <a href=" " style={{ lineHeight: "2.5" }}>
+            NRI Account Opening
+          </a>
+          <br />
+          <a href=" " style={{ lineHeight: "2.5" }}>
+            Online Account Opening
+          </a>
+          <br />
+          <a href=" " style={{ lineHeight: "2.5" }}>
+            Charges at Zerodha
+          </a>
+          <br />
+          <a href=" " style={{ lineHeight: "2.5" }}>
+            Zerodha IDFC FIRST Bank 3-in-1 Account
+          </a>
+          <br />
+          <a href=" " style={{ lineHeight: "2.5" }}>
+            Getting Started
+          </a>
+          <br />
+        </div>
+        <div className="col-4 p-5 mt-2 mb-2">
+          <h4 className="">
+            <i className="fa fa-plus-circle" aria-hidden="true"></i>
+            Your Zerodha Account
+          </h4>
+          <a href=" " style={{ lineHeight: "2.5" }}>
+            Login Credentials
+          </a>
+          <br />
+          <a href=" " style={{ lineHeight: "2.5" }}>
+            Account Modification and Segment Addition
+          </a>
+          <br />
+          <a href=" " style={{ lineHeight: "2.5" }}>
+            DP ID and bank details
+          </a>
+          <br />
+          <a href=" " style={{ lineHeight: "2.5" }}>
+            Your Profile
+          </a>
+          <br />
+          <a href=" " style={{ lineHeight: "2.5" }}>
+            Transfer and conversion of shares
+          </a>
+          <br />
+        </div>
+        <div className="col-4 p-5 mt-2 mb-2">
+          <h4 className="">
+            <i className="fa fa-plus-circle" aria-hidden="true"></i>
+            Your Zerodha Account
+          </h4>
+          <a href=" " style={{ lineHeight: "2.5" }}>
+            Margin/leverage, Product and Order types
+          </a>
+          <br />
+          <a href=" " style={{ lineHeight: "2.5" }}>
+            Kite Web and Mobile
+          </a>
+          <br />
+          <a href=" " style={{ lineHeight: "2.5" }}>
+            Trading FAQsg
+          </a>
+          <br />
+          <a href=" " style={{ lineHeight: "2.5" }}>
+            Corporate Actions
+          </a>
+          <br />
+          <a href=" " style={{ lineHeight: "2.5" }}>
+            Sentinel
+          </a>
+          <br />
+          <a href=" " style={{ lineHeight: "2.5" }}>
+            Kite API
+          </a>
+          <br />
+          <a href=" " style={{ lineHeight: "2.5" }}>
+            Stockreports+
+          </a>
+          <br />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default CreateTicket
+export default CreateTicket;
