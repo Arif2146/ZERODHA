bb12a543ef540942989578b1d59aeef80c044d88
Arif Bepari <arifbepari2006@gmail.com>
2026-02-11 18:07:49 +0530

    half hero support

--- frontend/src/landing/support/Hero.jsx
+++ frontend/src/landing/support/Hero.jsx
@@ -1,9 +1,12 @@
 function Hero() {
-    return (
-        <div>
-            <h1 className="text-3xl font-bold underline">Hero</h1>
-        </div>
-    );
+  return (
+    <section className="container-fluid" id="supportHero">
+      <div id="supportWrapper" className="p-5 mt-5 mb-5">
+        <h3>Support Portal</h3>
+        <a href=" ">Track Tickets</a>
+      </div>
+    </section>
+  );
 }
 
-export default Hero
+export default Hero;
